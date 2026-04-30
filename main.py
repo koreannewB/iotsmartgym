933ca2d46fc438e6c6ccd759432fd7d6b3aefd45
koreannewB <joeynoh2002@naver.com>
2026-04-30 14:27:27 +0900

    readmd추가

--- main.py
+++ main.py
@@ -11,27 +11,15 @@ from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 import time
 import threading
-
-app = FastAPI()
+import sensor
 from fastapi.staticfiles import StaticFiles
+    
+app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# 각 기능 3개가 동시에 작동하도록 스레드생성
-threadtreadmill = threading.Thread(target=treadmill.trail_detect_run)
-# threadtowel = threading.Thread(target=towel_remaining.run)
-# threadfitness = threading.Thread(target=fitness_equipment.run)
-
-threadtreadmill.daemon = True
-# threadtowel.daemon = True
-# threadfitness.daemon = True
-
-threadtreadmill.start()
-# threadtowel.start()
-# threadfitness.start()
 
 
-
-# 이파일실행될떄만 실행되도록 
+    # 이파일실행될떄만 실행되도록 
 @app.get('/')
 async def index():
     return FileResponse('templates/index.html')
@@ -43,3 +31,25 @@ async def tm_data():
         "Tmn3": state.TREADMILL[3],
         "Tmn4": state.TREADMILL[4]
     }
+
+def main():
+        
+    sensor.init_gpio()
+    sensor.start_threads()
+
+    # 각 기능 3개가 동시에 작동하도록 스레드생성
+    threadtreadmill = threading.Thread(target=treadmill.trail_detect_run)
+    # threadtowel = threading.Thread(target=towel_remaining.run)
+    # threadfitness = threading.Thread(target=fitness_equipment.run)
+
+    threadtreadmill.daemon = True
+    # threadtowel.daemon = True
+    # threadfitness.daemon = True
+
+    threadtreadmill.start()
+    # threadtowel.start()
+    # threadfitness.start()
+
+
+if __name__ == "__main__":
+    main()
